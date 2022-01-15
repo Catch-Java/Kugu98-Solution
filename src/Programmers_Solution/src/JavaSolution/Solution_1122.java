79e762ef55d53df67cafd6f896b2c9be4d499390
kugu98 <82009882+kugu98@users.noreply.github.com>
2022-01-15 11:03:31 +0900

    programmers

---
+++ src/Programmers_Solution/src/JavaSolution/Solution_1122.java
@@ -0,0 +1,25 @@
+package Programmers_Solution;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution_1122 {
+    public int[] solution(int[] numbers){
+        ArrayList<Integer> list = new ArrayList<>();
+        int sum=0;
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                sum=numbers[i]+numbers[j];
+                if(!list.contains(sum)){
+                    list.add(sum);
+                }
+            }
+        }
+        int [] answer=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]= (int) list.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+
+}
